dc59915e7d15dd1ab028f4e506fc7957e83d3916
han-young00 <wsucow@msn.com>
2025-10-03 09:29:14 +0900

    python  오퍼레이터

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,23 @@
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk import DAG, chain
+import random
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule=None,
+    start_date=pendulum.datetime(2025, 10, 2, tz="UTC"),
+    catchup=False,
+    tags=["example", "example2", "example3"],
+) as dag:
+    def select_fruit():
+        fruit = ['APPLE','BANANA', 'ORANGE']
+        rend_int = random.randint(0,3)
+        print(fruit[rand_int])
+    
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=select_fruit
+    )
+
+    py_t1
